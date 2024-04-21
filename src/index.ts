c9f58d977cd8ef1e8b9e68d2d016d26fdb65eb2d
Bruno Denardo <brunoden.bd@gmail.com>
2024-04-21 20:54:00 -0300

    Config: conexao com o banco de dados

--- src/index.ts
+++ src/index.ts
@@ -1,1 +1,11 @@
-//teste2
+import express from "express";
+import { config } from 'dotenv';
+
+config();
+const app = express();
+app.use(express.json());
+
+const PORT = process.env.PORT;
+app.listen(PORT, () => {
+  console.log(`Servidor rodando na porta ${PORT}`);
+});
