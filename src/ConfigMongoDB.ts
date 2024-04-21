21a9ab7200a4c68f801c3e40e7d2e6964784077a
Bruno Denardo <112097038+brunodenardo@users.noreply.github.com>
2024-04-21 20:56:33 -0300

    Config: conexao com o banco de dados (#4)

---
+++ src/ConfigMongoDB.ts
@@ -0,0 +1,19 @@
+import { MongoClient, ServerApiVersion } from "mongodb";
+import { config } from "dotenv";
+config();
+
+const uri = process.env.DB_URL;
+
+if(uri==undefined){
+    throw new Error("URI do MongoDB não configurada no arquivo .env")
+}
+
+const MongoDB = new MongoClient(uri, {
+    serverApi: {
+      version: ServerApiVersion.v1,
+      strict: true,
+      deprecationErrors: true,
+    }
+  });
+
+export default MongoDB;
